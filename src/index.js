8569055d2448c7e1deaf540c885016a3292737c6
Anna S <anna_sfe@hotmail.com>
2022-11-21 14:35:49 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,16 @@
+
+function sumArray(array){
+    let result;
+    array.forEach(item => {
+        if(Number.isInteger(item)) {
+            result=result+item;
+        } else {
+            return result=NaN;
+        }
+    });
+
+    return result;
+}
+
+
+module.exports = sumArray;
---
+++ test/sum.test.js
@@ -0,0 +1,29 @@
+const letterCount = require("../src/sum");
+
+describe("Testing function letterCount", () => {
+  test("returns object from string by counting letters", () => {
+    expect(letterCount("h")).toStrictEqual({ h: 1 });
+    expect(letterCount("hello")).toStrictEqual({ h: 1, e: 1, l: 2, o: 1 });
+    expect(letterCount("eeeee")).toStrictEqual({ e: 5 });
+  });
+
+  test("empty string must return empty object", () => {
+    expect(letterCount("")).toStrictEqual({});
+  });
+
+  test("spaces in string must be ignored", () => {
+    expect(letterCount("hello world")).toStrictEqual({
+      h: 1,
+      e: 1,
+      l: 3,
+      o: 2,
+      w: 1,
+      r: 1,
+      d: 1,
+    });
+  });
+
+  test("when input is not a string return empty object", () => {
+    expect(letterCount(1)).toStrictEqual({});
+  });
+});
